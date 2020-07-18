5e9f370a3bd415da7261900a10d78819d9f7f347
alexandercherniak <padre.12028@gmail.com>
2020-07-18 11:09:23 +0300

    remade

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-let a = +prompt('Число: ');
-outerloop: for (let i = 2; i <= a; i++) {
-  innerloop: for (let j = 2; j < i; j++) {
-    if (i % j == 0) {
-      continue outerloop; 
-    } 
-  } console.log (i);
+let a = +prompt('Введите число: ');
+
+let counter = 0;
+for (let i = 2; i < a; i++) {
+  if (a % i == 0) {
+    counter ++;
+    break;
+  } 
 }
 
+if (counter > 0) { 
+  console.log (+ a +' ' +'составное число');
+  } else { console.log (+ a +' ' +'простое число');
+}
+
+for (let i = 2; i <= 1000; i++) {
+  let flag = true;
+  for (let j = 2; j < i; j++) {
+    if (i % j == 0) {
+      flag = false;
+    }
+  } 
+  if (flag == true) {
+    console.log (i);
+  }
+}
+
+
+
+// v_2
+// outerloop: for (let i = 2; i <=1000; i++) {
+//   innerloop: for (let j = 2; j < i; j++) {
+//     if (i % j == 0) {
+//       continue outerloop;
+//     }
+//   } console.log (i);
+// }
